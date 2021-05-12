5baa84575d638e772a80af7b1a6cd19c7c252d73
bgeorgieff <b.g.georgiev87@gmail.com>
2021-05-12 12:29:53 +0300

    feat: added origami container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/header/index'
 import styles from './app.module.css'
 import Aside from './components/aside'
+import Posts from './components/posts'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <div className={styles.container}>
         <Aside />
+        <Posts />
       </div>
     </div>
   )
