0150e19afb8946b4d9aa73f012b93584c059dfdf
bgeorgieff <b.g.georgiev87@gmail.com>
2021-05-12 11:19:57 +0300

    feat: created aside with styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './components/header/index'
 import styles from './app.module.css'
+import Aside from './components/aside'
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <div className={styles.container}>
+        <Aside />
+      </div>
     </div>
   )
 }
